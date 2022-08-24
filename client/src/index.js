75169099f0ed977d0d7becea38ac62f9b69b2f6e
paffen <pavel.stoyanov0@hm.edu>
2022-08-24 16:42:03 +0200

    update code to React 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore,applyMiddleware, compose } from 'redux';
+//import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+//import { Provider } from 'react-redux';
+//import { createStore,applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
-import reducers from './reducers';
+//import reducers from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-    reducers,
-    composeEnhancers(applyMiddleware())
-    );
+// const store = createStore(
+//     reducers,
+//     composeEnhancers(applyMiddleware())
+//     );
 
-ReactDOM.render(
-    <Provider store= {store} >  
-    <App />
-    </Provider>,
-    document.querySelector('#root') 
-); 
+const container = document.getElementById('root');
+
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="root" />);
+
+// ReactDOM.render(
+//   //  <Provider store= {store} >  
+//     <App />,
+//   //  </Provider>,
+//     document.querySelector('#root') 
+// ); 
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import authReducer from '../reducers';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer 
 });
